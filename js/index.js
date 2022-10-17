1c670eb6503ff0434e35ded73bedbabe5ce65ee6
Oscar Mattsson <oscar.mattsson.790@gmail.com>
2022-10-17 21:40:44 +0200

    not done

---
+++ js/index.js
@@ -0,0 +1,42 @@
+// const buttonElem = document.querySelector("#button");
+// function add(firstNumber, secondNumber) {
+//   const sum = firstNumber + secondNumber;
+
+//   return sum;
+// }
+
+// function showSum(sum) {
+//   console.log(`Summan är: ${sum}`); // template strings
+// }
+
+// function start() {
+//   const sumToAdd = add(5, 5);
+
+//   showSum(sumToAdd); // Värdet i variablen (sumToAdd) som vi skickar med
+// }
+
+// start();
+
+// buttonElem.addEventListener("click", () => {});
+
+// ÖVNING 1
+// let str = ["hej", "på", "dig", "vad", "händer?"];
+// function getLength(str) {
+//   return str.length;
+// }
+// console.log(str.length);
+
+// ÖVNING 2
+// let year = "2019-10-14";
+// function getYear(year) {
+//   return year;
+// }
+// console.log(year.substring(0, 4));
+
+// Övning 3
+
+function addTwoNumbers(num1 + num2) {
+  const sum = num1 + num2;
+  return sum;
+}
+
